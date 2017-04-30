8799682608c0649e5e2f5f405288565dbf7526e3
lynachen <lingna.chen@outlook.com>
2017-04-30 23:07:00 +0800

    Update myshoppingcart.js

--- scripts/myshoppingcart.js
+++ scripts/myshoppingcart.js
@@ -1,11 +1,9 @@
 $(function(){
-    //下边框移动：
 	$(".switch-cart li").hover(function(){
 		$(this).css("borderBottom","2px solid #f40").siblings().css("borderBottom","none");
 		$(this).addClass("selectColumn").siblings().removeClass("selectColumn");
 	});
 
-	//显示卖家促销下拉列表：
 
 	$(".promotion").mouseover(function(){
 		$(this).siblings(".proSlidedown").show("fast");
@@ -14,14 +12,13 @@ $(function(){
 		$(this).siblings(".proSlidedown").hide();
 	});
 
-	//数量加
 	$(".add").click(function(){
 		var t=$(this).parent().find(".count-input");
 		t.val(parseInt(t.val())+1);
 		console.log("+:",t.val());
 		TotalPrice();
 	});
-	//数量减
+	
 	$(".reduce").click(function(){
 		var t=$(this).parent().find(".count-input");
 		t.val(parseInt(t.val())-1);
@@ -32,61 +29,60 @@ $(function(){
 		console.log("-:",t.val());
 	});
 
-	//点击商品按钮
+
 	$(".GoodsCheck").click(function(){
-		var goods=$(this).closest(".mainCommodity").find(".GoodsCheck");   //获取本店铺的所有商品
-		var goodsC=$(this).closest(".mainCommodity").find(".GoodsCheck:checked");  //获取本店铺所有被选中的商品 
-		var Shops=$(this).closest(".mainCommodity").find(".shopMsg-input");  //获取本店铺的全选按钮 
-		if(goodsC.length==goods.length){   //如果选中的商品等于所有商品
-			Shops.prop("checked",true);      //店铺全选按钮被选中 
-			if($(".shopMsg-input").length==$(".shopMsg-input:checked").length){  //如果店铺被选中的数量等于所有店铺的数量 
-				$("#selectAll").prop("checked",true);   //全选按钮被选中
+		var goods=$(this).closest(".mainCommodity").find(".GoodsCheck");   
+		var goodsC=$(this).closest(".mainCommodity").find(".GoodsCheck:checked");  
+		var Shops=$(this).closest(".mainCommodity").find(".shopMsg-input");  
+		if(goodsC.length==goods.length){   
+			Shops.prop("checked",true);      
+			if($(".shopMsg-input").length==$(".shopMsg-input:checked").length){  
+				$("#selectAll").prop("checked",true);   
 				TotalPrice();
 			}else{
-				$("#selectAll").prop("checked",false);  //全选按钮不被选中 
+				$("#selectAll").prop("checked",false);  
 				TotalPrice();
 			}
-		}else{   //如果选中的商品不等于所有商品 
-			Shops.prop("checked",false);   //店铺全选按钮不被选中 
-			$("#selectAll").prop("checked",false);  //全选按钮也不被选中
+		}else{  
+			Shops.prop("checked",false);   
+			$("#selectAll").prop("checked",false); 
 			TotalPrice(); 
 		}
 	});
 
-	// 点击店铺按钮 
 	$(".shopMsg-input").click(function(){
-		if($(this).prop("checked")){   //如果店铺按钮被选中
-			$(this).parents(".mainCommodity").find(".goods-check").prop("checked",true); //店铺内的所有商品按钮也被选中 
+		if($(this).prop("checked")){   
+			$(this).parents(".mainCommodity").find(".goods-check").prop("checked",true); 
 			console.log("DIANPU:",$(".shopMsg-input").length+"---"+$(".shopMsg-input:checked").length);
-			if($(".shopMsg-input").length==$(".shopMsg-input:checked").length){  //如果店铺被选中的数量等于所有店铺的数量
-				$("#selectAll").prop("checked",true);   //全选按钮被选中 
+			if($(".shopMsg-input").length==$(".shopMsg-input:checked").length){  
+				$("#selectAll").prop("checked",true);  
 				TotalPrice();
 			}else{
-				$("#selectAll").prop("checked",false);  //全选按钮不被选中 
+				$("#selectAll").prop("checked",false);  
 				TotalPrice();
 			}
-		}else{  //如果店铺按钮不被选中 
-			$(this).parents(".mainCommodity").find(".goods-check").prop("checked",false); //店铺内的所有商品按钮将不会被全选 
-			$("#selectAll").prop("checked",false);    //全选按钮也不被选中 
+		}else{  
+			$(this).parents(".mainCommodity").find(".goods-check").prop("checked",false); 
+			$("#selectAll").prop("checked",false);   
 			TotalPrice();
 		}
 	});
 
-	// 点击全选按钮 
+	
 	$(".allSelected").click(function(){
 		console.log("click");
-		if($(this).prop("checked")){   //如果全选按钮被选中
-			$(".goods-check").prop("checked",true); //所有按钮都被选中 
+		if($(this).prop("checked")){  
+			$(".goods-check").prop("checked",true);
 			TotalPrice();
 		}else{
-			$(".goods-check").prop("checked",false);  //所有按钮都被不选 
+			$(".goods-check").prop("checked",false);  
 			TotalPrice();
 		}
-		$(".shopMsg-input").change();//执行店铺全选的操作  
+		$(".shopMsg-input").change(); 
 	});
 	
 	$(".goods-check").click(function(){
-		if($(this).is(":checked")){  //如果商品被选中
+		if($(this).is(":checked")){ 
 			$(".btn-common").css({
 				"background-color":"#FF4400",
 				"cursor":"pointer"
@@ -101,16 +97,16 @@ $(function(){
 
 
 	function TotalPrice(){
-		var oprice=0;  //总价
-		var onum=0;  //数量
-		$(".mainCommodity .commodityInfo .GoodsCheck").each(function(){  //循环店铺里面的商品 
-			if($(this).is(":checked")){  //如果该商品被选中 
-				var num=parseInt($(this).parents(".commodityInfo").find("input[class*=count-input]").val()); //得到商品的数量
+		var oprice=0;  
+		var onum=0;  
+		$(".mainCommodity .commodityInfo .GoodsCheck").each(function(){  
+			if($(this).is(":checked")){  
+				var num=parseInt($(this).parents(".commodityInfo").find("input[class*=count-input]").val()); 
 				//console.log("数量：",num);
-				var price=parseFloat($(this).parents(".commodityInfo").find(".unit-price").text()); //得到商品的单价 
+				var price=parseFloat($(this).parents(".commodityInfo").find(".unit-price").text()); 
 				//console.log("单价：",price);
-				var total=price*num;  //计算单个商品的总价
-				oprice+=parseFloat(total);  //计算该店铺的总价 
+				var total=price*num;  
+				oprice+=parseFloat(total);  
 				onum+=parseInt(num);
 			}
 			$(".total-amount").text(onum);
